65d4d86f8ddcd0442552cf8407874f3f85876792
htbkoo <h1017760@connect.hku.hk>
2019-02-04 18:06:38 +0800

    extracted `<PortfolioPage/>` to separate components htbkoo/personal-portfolio

--- src/PortfolioDrawer.tsx
+++ src/PortfolioDrawer.tsx
@@ -1,30 +1,18 @@
 import React from 'react';
 import {Theme, withStyles} from '@material-ui/core/styles';
 import {createStyles, WithStyles} from '@material-ui/core';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import PortfolioAppBar from "./PortfolioAppBar";
-import PortfolioDrawer from "./PortfolioDrawer";
-import PortfolioMain from "./PortfolioMain";
+import Drawer from '@material-ui/core/Drawer';
+import DrawerItemsWithScrollspy from "./DrawerItemsWithScrollspy";
 
 const drawerWidth = 240;
 
 const styles = (theme: Theme) => createStyles({
-    root: {
-        display: 'flex',
-    },
-    appBar: {
-        zIndex: theme.zIndex.drawer + 1,
-    },
     drawer: {
         width: drawerWidth,
         flexShrink: 0,
     },
     drawerPaper: {
         width: drawerWidth,
-    },
-    content: {
-        flexGrow: 1,
-        padding: theme.spacing.unit * 3,
     },
     toolbar: theme.mixins.toolbar,
 });
@@ -36,12 +24,18 @@ function PortfolioPage(props: PortfolioPageProps) {
     const {classes} = props;
 
     return (
-        <div className={classes.root}>
-            <CssBaseline/>
-            <PortfolioAppBar/>
-            <PortfolioDrawer/>
-            <PortfolioMain/>
-        </div>
+        <Drawer
+            className={classes.drawer}
+            variant="permanent"
+            classes={{
+                paper: classes.drawerPaper,
+            }}
+        >
+            <div className={classes.toolbar}/>
+
+            <DrawerItemsWithScrollspy items={['Home', 'About', 'Portfolio', 'Contact']}/>
+
+        </Drawer>
     );
 }
 
