f6433b93ff157d8207eea7256455471e9d44ceed
htbkoo <h1017760@connect.hku.hk>
2019-02-11 23:39:03 +0800

    created and provided MUI Theme for application htbkoo/personal-portfolio

---
+++ src/services/MuiThemeFactory.ts
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import {blue, indigo} from "@material-ui/core/colors";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: blue,
+        secondary: indigo,
+    },
+});
+
+export {theme};
