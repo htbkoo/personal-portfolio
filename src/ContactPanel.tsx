0865f7639c61db3a0177b8051cc4ebaef84f5ec7
htbkoo <h1017760@connect.hku.hk>
2019-02-12 00:39:38 +0800

    added contactIcons and contactBadge htbkoo/personal-portfolio

--- src/ContactPanel.tsx
+++ src/ContactPanel.tsx
@@ -1,55 +1,63 @@
 import * as React from "react";
 import {createStyles, Theme, withStyles, WithStyles} from "@material-ui/core";
-import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 
-const styles = (theme: Theme) => createStyles({});
+import iconMetadatas from "./metadata/contact/icons.json";
+
+const styles = (theme: Theme) => createStyles({
+    icons: {
+        padding: theme.spacing.unit * 3,
+        display: "flex",
+        flexFlow: "wrap",
+        justifyContent: "center",
+    },
+    icon: {
+        padding: theme.spacing.unit * 2,
+    }
+});
 
 interface ContactPanelProps extends WithStyles<typeof styles> {
 }
 
 function ContactPanel(props: ContactPanelProps) {
-    return <div id="contact">
-        <Typography paragraph >
-            Contact Panel
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-            ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-            facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-            gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-            donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-            adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-            Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-            imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-            arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-            donec massa sapien faucibus et molestie ac.
-            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-            facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-            tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-            consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-            vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-            hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-            tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-            nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-            accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-            facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-            tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-            consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-            vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-            hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-            tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-            nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-            accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-            facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-            tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-            consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-            vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-            hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-            tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-            nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-            accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-    </div>;
+    const {classes} = props;
+    return (
+        <div id="contact">
+            <div>
+                <h1>Contact</h1>
+                <h2>Check me out at the following!</h2>
+            </div>
+            <Divider/>
+            <div className={classes.icons}>
+                {contactIcons({classes})}
+            </div>
+            <div className="contact-badges">
+                <div>
+                    <a href="https://www.codewars.com/users/htbkoo" target="_blank">
+                        <img
+                            src="https://www.codewars.com/users/htbkoo/badges/large"
+                            alt="Codewars"/>
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function contactIcons({classes}) {
+    return iconMetadatas.map(toIconComponent);
+
+    function toIconComponent(metadata) {
+        return (
+            <div className={classes.icon}>
+                <a href={metadata.href} target="_blank">
+                    <img
+                        src={metadata.img.src}
+                        alt={metadata.img.alt}/>
+                </a>
+            </div>
+        )
+    }
 }
 
 export default withStyles(styles)(ContactPanel);
