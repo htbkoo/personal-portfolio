cb801ba283de8db0a820661f8a9d3894750858b3
htbkoo <h1017760@connect.hku.hk>
2019-02-12 12:25:23 +0800

    added about information and created `<AboutInformation/>` htbkoo/personal-portfolio

--- src/components/about/AboutPanel.tsx
+++ src/components/about/AboutPanel.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import {createStyles, Theme, withStyles, WithStyles} from "@material-ui/core";
-import Typography from '@material-ui/core/Typography';
 
 import Section from "../common/Section";
+import AboutInformation from "./AboutInformation";
 
 const styles = (theme: Theme) => createStyles({});
 
@@ -13,23 +13,11 @@ function AboutPanel(props: AboutPanelProps) {
     return (
         <Section
             id="about"
-            hasDivider={true}
+            hasDivider={false}
             title="About"
             subtitle="Some information about myself"
         >
-            <Typography paragraph >
-                About Panel
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-                facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-                gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-                donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-                Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-                imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-                arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-                donec massa sapien faucibus et molestie ac.
-            </Typography>
+            <AboutInformation/>
         </Section>
     );
 }
