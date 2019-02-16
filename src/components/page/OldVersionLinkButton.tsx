9a47a8f311a9ea47430e014a43c12dd9d2e26940
htbkoo <h1017760@connect.hku.hk>
2019-02-16 16:25:38 +0800

    refactored to use MUI built-in support for `<Hidden/>` for old version link button (reference: https://material-ui.com/layout/hidden/) htbkoo/personal-portfolio

--- src/components/page/OldVersionLinkButton.tsx
+++ src/components/page/OldVersionLinkButton.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import {Theme, withStyles} from '@material-ui/core/styles';
 import {createStyles, WithStyles} from '@material-ui/core';
 import Button from '@material-ui/core/Button';
-import {onlyDisplayIfWidthAtLeast} from "../../css/stylesHelpers/conditionalDisplay";
+import Hidden from '@material-ui/core/Hidden';
 
 const styles = (theme: Theme) => createStyles({
-    linkButton: onlyDisplayIfWidthAtLeast(theme, 'sm')
 });
 
 interface OldVersionLinkButtonProps extends WithStyles<typeof styles> {
@@ -14,11 +13,12 @@ interface OldVersionLinkButtonProps extends WithStyles<typeof styles> {
 const OLD_VERSION_URL = "https://codepen.io/htbkoo/full/ZyYWNN/";
 
 function OldVersionLinkButton(props: OldVersionLinkButtonProps) {
-    const {classes} = props;
     return (
-        <Button href={OLD_VERSION_URL} variant="contained" color="inherit" className={classes.linkButton}>
-            Switch To old version
-        </Button>
+        <Hidden xsDown>
+            <Button href={OLD_VERSION_URL} variant="contained" color="inherit">
+                Switch To old version
+            </Button>
+        </Hidden>
     );
 }
 
