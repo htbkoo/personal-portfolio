d83022ecc1a4695d58c46aba7a69d7e4df60d587
htbkoo <h1017760@connect.hku.hk>
2019-02-16 16:09:12 +0800

    updated design (not showing link button to old version for small screen) htbkoo/personal-portfolio

--- src/components/page/OldVersionLinkButton.tsx
+++ src/components/page/OldVersionLinkButton.tsx
@@ -4,9 +4,19 @@ import {createStyles, WithStyles} from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 
 const styles = (theme: Theme) => createStyles({
-    appBar: {
-        zIndex: theme.zIndex.drawer + 1,
-    },
+    linkButton: {
+        display: "none",
+        // Match [sm, ∞[
+        //       [600px, ∞[
+        [theme.breakpoints.up('sm')]: {
+            display: "unset",
+        },
+        // Match [md, ∞[
+        //       [960px, ∞[
+        [theme.breakpoints.up('md')]: {
+            display: "unset",
+        },
+    }
 });
 
 interface OldVersionLinkButtonProps extends WithStyles<typeof styles> {
@@ -15,8 +25,11 @@ interface OldVersionLinkButtonProps extends WithStyles<typeof styles> {
 const OLD_VERSION_URL = "https://codepen.io/htbkoo/full/ZyYWNN/";
 
 function OldVersionLinkButton(props: OldVersionLinkButtonProps) {
+    const {classes} = props;
     return (
-        <Button href={OLD_VERSION_URL} variant="contained" color="inherit">Switch To old version</Button>
+        <Button href={OLD_VERSION_URL} variant="contained" color="inherit" className={classes.linkButton}>
+            Switch To old version
+        </Button>
     );
 }
 
