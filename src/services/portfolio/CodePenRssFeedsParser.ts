77d8392322078938c0322e7a8d15f9b63a21a17d
htbkoo <h1017760@connect.hku.hk>
2019-02-25 01:10:47 +0800

    extracted interface `RssFeedsParser` htbkoo/personal-portfolio

--- src/services/portfolio/CodePenRssFeedsParser.ts
+++ src/services/portfolio/CodePenRssFeedsParser.ts
@@ -1,11 +1,12 @@
 import {Output} from "rss-parser";
+import RssFeedsParser from "./RssFeedsParser";
 
 interface RssParser {
     // parseString(xml: string, callback?: (err: Error, feed: Output) => void): Promise<Output>;
     parseURL(feedUrl: string, callback?: (err: Error, feed: Output) => void, redirectCount?: number): Promise<Output>;
 }
 
-export default class CodePenRssFeedsParser {
+export default class CodePenRssFeedsParser implements RssFeedsParser {
     private readonly rssParser: RssParser;
 
     constructor(rssParser: RssParser) {
