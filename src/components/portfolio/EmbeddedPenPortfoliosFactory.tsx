228cd6a1390098b4cbca445b594af88744ca90eb
htbkoo <h1017760@connect.hku.hk>
2019-03-25 19:46:21 +0800

    refactored (extracted `<EmbeddedPenPortfolios/>`) htbkoo/personal-portfolio

--- src/components/portfolio/EmbeddedPenPortfoliosFactory.tsx
+++ src/components/portfolio/EmbeddedPenPortfoliosFactory.tsx
@@ -1,10 +1,9 @@
 import * as React from "react";
-import {Items} from "rss-parser";
 import {CodepenEmbedScriptTagBuilder, ScriptTagBuilder} from "responsive-react-codepen-embed";
 
 import RssFeedsParser from "../../services/portfolio/RssFeedsParser";
 import {PortfoliosFactory} from "./PortfoliosFactory";
-import EmbeddedPenPortfolio from "./EmbeddedPenPortfolio";
+import EmbeddedPenPortfolios from "./EmbeddedPenPortfolios";
 
 export default class EmbeddedPenPortfoliosFactory implements PortfoliosFactory {
     private readonly parser: RssFeedsParser;
@@ -16,54 +15,6 @@ export default class EmbeddedPenPortfoliosFactory implements PortfoliosFactory {
     }
 
     createPortfolios() {
-        return Promise.resolve(<PenPortfolios parser={this.parser} rssFeedUrl={this.rssFeedUrl}/>);
-    }
-}
-
-interface PenPortfoliosProps {
-    parser: RssFeedsParser;
-    rssFeedUrl: string;
-}
-
-interface PenPortfoliosState {
-    items: Items[],
-    loaded: boolean,
-    error?: string,
-}
-
-class PenPortfolios extends React.Component<PenPortfoliosProps, PenPortfoliosState> {
-    private scriptTagBuilder: ScriptTagBuilder;
-
-    constructor(props: Readonly<PenPortfoliosProps>) {
-        super(props);
-        this.state = {loaded: false, items: []};
-
-        this.scriptTagBuilder = new CodepenEmbedScriptTagBuilder()
-            .setAsync(true)
-            .withOnLoadHandler(() => this.setState({loaded: true,}))
-            .withOnErrorHandler(() => this.setState({error: 'Failed to load the pen'}));
-    }
-
-    componentDidMount(): void {
-        this.props.parser.parseUrl(this.props.rssFeedUrl)
-            .then(items => this.setState({items}))
-            .then(() => this.scriptTagBuilder.appendTo(document.body))
-            .catch(error => console.log(error));
-    }
-
-    render() {
-        return (
-            <div>
-                {this.state.items.map(({content = "", link = "", title = ""}: Items, index: number) =>
-                    (<EmbeddedPenPortfolio
-                        key={`${index}_${title}`}
-                        content={content}
-                        link={link}
-                        title={title}
-                        isScriptLoaded={this.state.loaded}
-                    />)
-                )}
-            </div>
-        );
+        return Promise.resolve(<EmbeddedPenPortfolios parser={this.parser} rssFeedUrl={this.rssFeedUrl}/>);
     }
 }
