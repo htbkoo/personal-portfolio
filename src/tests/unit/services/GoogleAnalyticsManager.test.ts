ad4495327e53a71dc757248a2572ccfb76987ed2
htbkoo <h1017760@connect.hku.hk>
2019-04-03 10:35:40 +0800

    updated tests grouping htbkoo/personal-portfolio

--- src/tests/unit/services/GoogleAnalyticsManager.test.ts
+++ src/tests/unit/services/GoogleAnalyticsManager.test.ts
@@ -14,47 +14,49 @@ describe("GoogleAnalyticsManager", function () {
         process.env = processEnv;
     });
 
+    describe("initialize", function () {
+        it("should init if tracking is enabled and tracking id is provided", function () {
+            // given
+            overrideProcessEnv({trackingEnabled: ENABLED, trackingId: "someId"});
+            const manager = new GoogleAnalyticsManager();
+
+            // when
+            manager.init();
+
+            // then
+            expect(manager.isInitialized()).toEqual(true);
+            expect(ReactGA.initialize).toBeCalled();
+        });
+
+        it("should not init if tracking is disabled", function () {
+            // given
+            overrideProcessEnv({trackingEnabled: "false", trackingId: "someId"});
+            const manager = new GoogleAnalyticsManager();
+
+            // when
+            manager.init();
+
+            // then
+            expect(manager.isInitialized()).toEqual(false);
+            expect(ReactGA.initialize).not.toBeCalled();
+        });
+
+        it("should not init if tracking id is not provided", function () {
+            // given
+            overrideProcessEnv({trackingEnabled: ENABLED});
+            const manager = new GoogleAnalyticsManager();
+
+            // when
+            manager.init();
+
+            // then
+            expect(manager.isInitialized()).toEqual(false);
+            expect(ReactGA.initialize).not.toBeCalled();
+        });
+    });
+
     function overrideProcessEnv({trackingEnabled, trackingId}: { trackingEnabled?: string; trackingId?: string }) {
         process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ENABLED = trackingEnabled;
         process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID = trackingId;
     }
-
-    it("should init if tracking is enabled and tracking id is provided", function () {
-        // given
-        overrideProcessEnv({trackingEnabled: ENABLED, trackingId: "someId"});
-        const manager = new GoogleAnalyticsManager();
-
-        // when
-        manager.init();
-
-        // then
-        expect(manager.isInitialized()).toEqual(true);
-        expect(ReactGA.initialize).toBeCalled();
-    });
-
-    it("should not init if tracking is disabled", function () {
-        // given
-        overrideProcessEnv({trackingEnabled: "false", trackingId: "someId"});
-        const manager = new GoogleAnalyticsManager();
-
-        // when
-        manager.init();
-
-        // then
-        expect(manager.isInitialized()).toEqual(false);
-        expect(ReactGA.initialize).not.toBeCalled();
-    });
-
-    it("should not init if tracking id is not provided", function () {
-        // given
-        overrideProcessEnv({trackingEnabled: ENABLED});
-        const manager = new GoogleAnalyticsManager();
-
-        // when
-        manager.init();
-
-        // then
-        expect(manager.isInitialized()).toEqual(false);
-        expect(ReactGA.initialize).not.toBeCalled();
-    });
 });
