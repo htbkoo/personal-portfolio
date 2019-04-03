78f1cf0c6c1f945a80e55f0b3b8344b6b7b0079a
htbkoo <h1017760@connect.hku.hk>
2019-04-03 08:50:08 +0800

    added automatic mock generation (reference: https://jestjs.io/docs/en/manual-mocks); spiked manual interaction handling htbkoo/personal-portfolio

--- __mocks__/react-ga.ts
+++ __mocks__/react-ga.ts
@@ -1,5 +1,29 @@
-export default {
-    initialize(trackingCode: string){
-        console.debug(`initialize with trackingCode: "${trackingCode}"`);
+import ReactGA from 'react-ga';
+
+const reactGA = jest.genMockFromModule<typeof ReactGA>('react-ga');
+
+let interactions = emptyInteractions();
+
+reactGA.initialize = function (trackingCode) {
+    console.debug(`initialize with trackingCode: "${trackingCode}"`);
+    interactions.initialize.push(trackingCode);
+};
+
+reactGA.pageview = function (path) {
+    console.debug(`pageview with path: "${path}"`);
+    interactions.pageView.push(path);
+};
+
+(reactGA as any).__resetInteractionCount = function () {
+    interactions = emptyInteractions();
+};
+
+function emptyInteractions() {
+    return {
+        initialize: [] as string[],
+        pageView: [] as string[],
     }
 }
+
+// noinspection JSUnusedGlobalSymbols
+export default reactGA;
