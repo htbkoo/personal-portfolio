25b49b22364c659d12134f7bd655f6b80348e33d
htbkoo <h1017760@connect.hku.hk>
2019-04-14 22:00:49 +0800

    added test coverage; added more meaningful error message

--- src/services/portfolio/LocalCodePenRssFeedsParser.ts
+++ src/services/portfolio/LocalCodePenRssFeedsParser.ts
@@ -15,16 +15,15 @@ export default class LocalCodePenRssFeedsParser implements RssFeedsParser {
         let output;
 
         try {
-            console.debug(`provided rssFeeds: ${this.rssFeeds}`);
             output = await this.rssParser.parseString(this.rssFeeds);
         } catch (e) {
-            throw new Error(`Unable to read or parse rssFeedsString due to ${e}`);
+            throw new Error(`Unable to read or parse rssFeedsString due to: '${e}'`);
         }
 
         if (output && output.items) {
             return output.items;
         } else {
-            throw new Error(`Missing 'items' from the parsed output: ${output}`);
+            throw new Error(`Missing 'items' from the parsed output: '${this.rssFeeds}'`);
         }
     }
 }
