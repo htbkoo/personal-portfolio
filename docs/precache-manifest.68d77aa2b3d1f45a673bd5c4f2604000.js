ad892368b494c9d82408fab537e44a482e781ab9
htbkoo <h1017760@connect.hku.hk>
2019-05-18 17:09:13 +0800

    rebuilt `docs/`

---
+++ docs/precache-manifest.68d77aa2b3d1f45a673bd5c4f2604000.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc41262401350ab4c027",
+    "url": "/personal-portfolio/static/js/runtime~main.fc412624.js"
+  },
+  {
+    "revision": "994cb85b8734561bb7ef",
+    "url": "/personal-portfolio/static/js/main.994cb85b.chunk.js"
+  },
+  {
+    "revision": "f1085861bf800937e351",
+    "url": "/personal-portfolio/static/js/1.f1085861.chunk.js"
+  },
+  {
+    "revision": "994cb85b8734561bb7ef",
+    "url": "/personal-portfolio/static/css/main.48d1f375.chunk.css"
+  },
+  {
+    "revision": "5f29487e681b04d182dbd0988bdd3d70",
+    "url": "/personal-portfolio/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/personal-portfolio/precache-manifest.7391b2a96ebf527e28acf156d97fe960.js"
+  "/personal-portfolio/precache-manifest.68d77aa2b3d1f45a673bd5c4f2604000.js"
 );
 
 workbox.clientsClaim();
