112b2b85b82d8f82ee2cb8953accd38cf86fb63b
htbkoo <h1017760@connect.hku.hk>
2021-02-14 15:41:44 +0800

    fixed broken test

--- src/tests/unit/components/App.test.tsx
+++ src/tests/unit/components/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../../../components/App';
+import { overrideGoogleAnalyticsRelatedProcessEnv } from "../../utils/utils";
 
 describe('<App/>', function () {
     it('renders without crashing', () => {
+        overrideGoogleAnalyticsRelatedProcessEnv({ trackingEnabled: "false" });
+
         const div = document.createElement('div');
         ReactDOM.render(<App/>, div);
         ReactDOM.unmountComponentAtNode(div);
