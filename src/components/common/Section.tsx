f699f3bdbf501f125f02245a01e1d121d4a59cb0
htbkoo <h1017760@connect.hku.hk>
2021-02-14 16:42:15 +0800

    (WIP) fixed page header overlapping in-page anchors issue
    References:
    1. https://stackoverflow.com/a/13117744
    2. https://css-tricks.com/hash-tag-links-padding/
    3. https://css-tricks.com/fixed-headers-on-page-links-and-overlapping-content-oh-my/
    4. https://css-tricks.com/link-header-header-link/
    5. http://nicolasgallagher.com/jump-links-and-viewport-positioning/)

--- src/components/common/Section.tsx
+++ src/components/common/Section.tsx
@@ -6,7 +6,9 @@ import Divider from '@material-ui/core/Divider';
 
 const styles = (theme: Theme) => createStyles({
     section: {
-        padding: theme.spacing(3)
+        padding: theme.spacing(3),
+        paddingTop: getPaddingTopForSection(theme),
+        marginTop: -getPaddingTopForSection(theme),
     },
     header: {},
     body: {},
@@ -50,6 +52,21 @@ function optionalDivider(hasDivider?: boolean) {
     return hasDivider
         ? <Divider/>
         : "";
+}
+
+function getPaddingTopForSection(theme: Theme): string | number {
+    // To fix the AppBar overlapping in-page anchors issue
+    // Reference: https://stackoverflow.com/a/13117744
+    const minHeight = theme.mixins.toolbar.minHeight;
+    if (minHeight) {
+        if (typeof minHeight === "number") {
+            return theme.spacing(3) + minHeight;
+        } else {
+            return minHeight;
+        }
+    } else {
+        return theme.spacing(3);
+    }
 }
 
 Section.defaultProps = {
