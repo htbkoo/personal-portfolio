31416f78551833065d69e4ea48bf619d3653c77d
htbkoo <h1017760@connect.hku.hk>
2021-03-15 03:13:28 +0800

    updated `<VersionText/>` to add `GitHub` `Octocat` icon and link to repo

--- src/components/common/VersionText.tsx
+++ src/components/common/VersionText.tsx
@@ -1,11 +1,33 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core";
 
+import { GitHubMarkImg } from "./GitHubMarkImg";
 import { version } from "../../../package.json";
 
+const LINK_TO_GITHUB_REPO = "https://github.com/htbkoo/personal-portfolio";
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        display: "flex",
+        flexDirection: "row",
+    },
+    linkIconContainer: {
+        marginLeft: theme.spacing(1),
+    },
+}));
+
 export function VersionText() {
+    const classes = useStyles();
     return (
-        <div>
-            v{version}-{process.env.REACT_APP_GIT_SHA}
-        </div>
+        <a href={LINK_TO_GITHUB_REPO} target="_blank" rel="noopener noreferrer">
+            <div className={classes.container}>
+                <div>
+                    v{version}-{process.env.REACT_APP_GIT_SHA}
+                </div>
+                <div className={classes.linkIconContainer}>
+                    <GitHubMarkImg />
+                </div>
+            </div>
+        </a>
     );
 }
