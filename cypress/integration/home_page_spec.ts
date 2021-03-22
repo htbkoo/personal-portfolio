557048c0e8570d9381cd5046dab80bdf3ae4200a
htbkoo <h1017760@connect.hku.hk>
2021-03-22 18:29:23 +0800

    added more tests case on links in `home_page_spec.ts`

--- cypress/integration/home_page_spec.ts
+++ cypress/integration/home_page_spec.ts
@@ -2,4 +2,21 @@ describe("Integration tests - Home Page", () => {
     it("successfully loads", () => {
         cy.visit("/");
     });
+
+    describe("links", () => {
+        beforeEach(() => {
+            cy.visit("/");
+        });
+
+        it("should jump to contact when clicked on Contact", () => {
+            // given
+            const linkEl = cy.get("a").contains("Contact");
+
+            // when
+            linkEl.click();
+
+            // then
+            cy.url().should("include", "#contact");
+        });
+    });
 });
