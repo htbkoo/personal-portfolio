9869a80a41025bbe2e315f8a7bb53ad9ac1b7fc5
htbkoo <h1017760@connect.hku.hk>
2021-05-30 15:37:38 +0800

    applied auto-formatting

--- src/services/MuiThemeFactory.ts
+++ src/services/MuiThemeFactory.ts
@@ -1,23 +1,24 @@
-import {createMuiTheme} from '@material-ui/core/styles';
-import {indigo} from "@material-ui/core/colors";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { indigo } from "@material-ui/core/colors";
 
 const theme = createMuiTheme({
     palette: {
         primary: indigo,
-        secondary: {main: '#e6f0fe',},
+        secondary: { main: "#e6f0fe" },
     },
-    typography: {
-    },
+    typography: {},
     overrides: {
         MuiTableRow: {
-            root: { //for the body
-                height: "100%"
+            root: {
+                //for the body
+                height: "100%",
             },
-            head: { //for the head
-                height: "100%"
-            }
-        }
-    }
+            head: {
+                //for the head
+                height: "100%",
+            },
+        },
+    },
 });
 
-export {theme};
+export { theme };
