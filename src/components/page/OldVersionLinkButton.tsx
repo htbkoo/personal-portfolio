15f707124bb4a4579b3d630cdc76406f98aaf2f3
htbkoo <h1017760@connect.hku.hk>
2021-05-30 18:09:27 +0800

    applied auto-formatting

--- src/components/page/OldVersionLinkButton.tsx
+++ src/components/page/OldVersionLinkButton.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import {Theme, withStyles} from '@material-ui/core/styles';
-import {createStyles, WithStyles} from '@material-ui/core';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import { Theme, withStyles } from "@material-ui/core/styles";
+import { createStyles, WithStyles } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
 
 const styles = (theme: Theme) => createStyles({});
 
-interface OldVersionLinkButtonProps extends WithStyles<typeof styles> {
-}
+interface OldVersionLinkButtonProps extends WithStyles<typeof styles> {}
 
 const OLD_VERSION_URL = "https://codepen.io/htbkoo/";
 
