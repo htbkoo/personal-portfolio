e210fbd530aa06be0134f2f86e3eafe5c46c12dd
htbkoo <h1017760@connect.hku.hk>
2021-05-30 15:51:23 +0800

    added support of using correct colour for `<GitHubMarkImg/>` based on theme

--- src/components/common/VersionText.tsx
+++ src/components/common/VersionText.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core";
+import { useTheme } from "@material-ui/core/styles";
 
 import { GitHubMarkImg } from "./GitHubMarkImg";
 import { version } from "../../../package.json";
@@ -18,6 +19,8 @@ const useStyles = makeStyles((theme) => ({
 
 export function VersionText() {
     const classes = useStyles();
+    const theme = useTheme();
+
     return (
         <a href={LINK_TO_GITHUB_REPO} target="_blank" rel="noopener noreferrer">
             <div className={classes.container}>
@@ -25,7 +28,7 @@ export function VersionText() {
                     v{version}-{process.env.REACT_APP_GIT_SHA}
                 </div>
                 <div className={classes.linkIconContainer}>
-                    <GitHubMarkImg />
+                    <GitHubMarkImg color={theme.palette.type === "dark" ? "white" : "black"} />
                 </div>
             </div>
         </a>
