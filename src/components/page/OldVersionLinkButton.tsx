e6226b2c64ee44a3ab53ead07cdd8ae4bdb79d47
htbkoo <h1017760@connect.hku.hk>
2021-05-30 18:10:28 +0800

    updated design

--- src/components/page/OldVersionLinkButton.tsx
+++ src/components/page/OldVersionLinkButton.tsx
@@ -3,15 +3,20 @@ import { Theme, withStyles } from "@material-ui/core/styles";
 import { createStyles, WithStyles } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 
-const styles = (theme: Theme) => createStyles({});
+const styles = (theme: Theme) =>
+    createStyles({
+        button: {
+            marginLeft: theme.spacing(1),
+        },
+    });
 
 interface OldVersionLinkButtonProps extends WithStyles<typeof styles> {}
 
 const OLD_VERSION_URL = "https://codepen.io/htbkoo/";
 
-function OldVersionLinkButton(props: OldVersionLinkButtonProps) {
+function OldVersionLinkButton({ classes }: OldVersionLinkButtonProps) {
     return (
-        <Button href={OLD_VERSION_URL} variant="contained" color="inherit">
+        <Button href={OLD_VERSION_URL} variant="contained" color="inherit" className={classes.button}>
             View on CodePen
         </Button>
     );
