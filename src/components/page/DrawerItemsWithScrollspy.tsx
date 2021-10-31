56668df89a61ea32cc4289703fdc2510258515ac
HeySP6 <h1017760@connect.hku.hk>
2021-10-31 18:27:48 +0100

    applied auto-formatting by `prettier`

--- src/components/page/DrawerItemsWithScrollspy.tsx
+++ src/components/page/DrawerItemsWithScrollspy.tsx
@@ -1,80 +1,80 @@
-import React from 'react';
-import {Theme, withStyles} from '@material-ui/core/styles';
-import {createStyles, WithStyles} from '@material-ui/core';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import Scrollspy from 'react-scrollspy';
-import Hidden from '@material-ui/core/Hidden';
+import React from "react";
+import { Theme, withStyles } from "@material-ui/core/styles";
+import { createStyles, WithStyles } from "@material-ui/core";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+import Scrollspy from "react-scrollspy";
+import Hidden from "@material-ui/core/Hidden";
 
 import OldVersionLinkButton from "./OldVersionLinkButton";
 
 const drawerWidth = 240;
 
-const styles = (theme: Theme) => createStyles({
-    root: {
-        display: 'flex',
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-    },
-    drawerPaper: {
-        width: drawerWidth,
-    },
-    content: {
-        flexGrow: 1,
-        padding: theme.spacing(3),
-    },
-    toolbar: theme.mixins.toolbar,
-    scrollSpyList: {
-        margin: 0,
-        padding: 0
-    },
-    scrollSpyListItem: {
-        display: 'flex',
-    },
-    oldVersionLinkButton: {
-        margin: "5%"
-    },
-    isCurrent: {
-        fontWeight: "bolder",
-        color: "#770077",
-        backgroundColor: "#dadaff"
-    }
-});
+const styles = (theme: Theme) =>
+    createStyles({
+        root: {
+            display: "flex",
+        },
+        drawer: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+        drawerPaper: {
+            width: drawerWidth,
+        },
+        content: {
+            flexGrow: 1,
+            padding: theme.spacing(3),
+        },
+        toolbar: theme.mixins.toolbar,
+        scrollSpyList: {
+            margin: 0,
+            padding: 0,
+        },
+        scrollSpyListItem: {
+            display: "flex",
+        },
+        oldVersionLinkButton: {
+            margin: "5%",
+        },
+        isCurrent: {
+            fontWeight: "bolder",
+            color: "#770077",
+            backgroundColor: "#dadaff",
+        },
+    });
 
 interface DrawerItemsWithScrollspyProps extends WithStyles<typeof styles> {
-    items: string[],
+    items: string[];
 }
 
 const EMPIRICAL_OFFSET = -80;
 
 function DrawerItemsWithScrollspy(props: DrawerItemsWithScrollspyProps) {
-    const {items, classes} = props;
+    const { items, classes } = props;
 
     return (
         <React.Fragment>
             <Scrollspy
-                items={items.map(item => item.toLowerCase())}  // TODO: refactor this
+                items={items.map((item) => item.toLowerCase())} // TODO: refactor this
                 currentClassName={classes.isCurrent}
                 className={classes.scrollSpyList}
-                offset={EMPIRICAL_OFFSET}
-            >
+                offset={EMPIRICAL_OFFSET}>
                 {items.map((text, index) => (
                     <a key={text} href={itemToHref(text)} className={classes.scrollSpyListItem}>
                         <ListItem button>
-                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}</ListItemIcon>
-                            <ListItemText primary={text}/>
+                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                            <ListItemText primary={text} />
                         </ListItem>
                     </a>
                 ))}
             </Scrollspy>
             <Hidden mdUp>
                 <div className={classes.oldVersionLinkButton}>
-                    <OldVersionLinkButton/>
+                    <OldVersionLinkButton />
                 </div>
             </Hidden>
         </React.Fragment>
