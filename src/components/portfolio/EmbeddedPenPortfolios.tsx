4e6acdd98b5b027b837ce835da5755f5a0b021da
HeySP6 <h1017760@connect.hku.hk>
2021-10-31 16:32:04 +0100

    refactored `<EmbeddedPenPortfolios/>` to functional components

--- src/components/portfolio/EmbeddedPenPortfolios.tsx
+++ src/components/portfolio/EmbeddedPenPortfolios.tsx
@@ -1,14 +1,13 @@
 import * as React from "react";
-import { createStyles, Theme, WithStyles, withStyles } from "@material-ui/core";
+import { useEffect, useState } from "react";
+
 import { Items } from "rss-parser";
-import { CodepenEmbedScriptTagBuilder, ScriptTagBuilder } from "ts-react-codepen-embed";
+import { CodepenEmbedScriptTagBuilder } from "ts-react-codepen-embed";
 
 import RssFeedsParser from "../../services/portfolio/RssFeedsParser";
 import EmbeddedPenPortfolio from "./EmbeddedPenPortfolio";
 
-const styles = (theme: Theme) => createStyles({});
-
-interface Props extends WithStyles<typeof styles> {
+interface Props {
     parser: RssFeedsParser;
     rssFeedUrl: string;
 }
@@ -19,42 +18,35 @@ interface State {
     error?: string;
 }
 
-class EmbeddedPenPortfolios extends React.Component<Props, State> {
-    private scriptTagBuilder: ScriptTagBuilder;
+export default ({ parser, rssFeedUrl }: Props) => {
+    const [loaded, setLoaded] = useState(false);
+    const [items, setItems] = useState<Items[]>([]);
+    const [error, setError] = useState<string | undefined>(undefined);
 
-    constructor(props: Readonly<Props>) {
-        super(props);
-        this.state = { loaded: false, items: [] };
+    const scriptTagBuilder = new CodepenEmbedScriptTagBuilder()
+        .setAsync(true)
+        .withOnLoadHandler(() => setLoaded(true))
+        .withOnErrorHandler(() => setError("Failed to load the pen"));
 
-        this.scriptTagBuilder = new CodepenEmbedScriptTagBuilder()
-            .setAsync(true)
-            .withOnLoadHandler(() => this.setState({ loaded: true }))
-            .withOnErrorHandler(() => this.setState({ error: "Failed to load the pen" }));
-    }
-
-    componentDidMount(): void {
-        this.props.parser
-            .parseUrl(this.props.rssFeedUrl)
-            .then((items) => this.setState({ items }))
-            .then(() => this.scriptTagBuilder.appendTo(document.body))
+    useEffect(() => {
+        parser
+            .parseUrl(rssFeedUrl)
+            .then((items) => setItems(items))
+            .then(() => scriptTagBuilder.appendTo(document.body))
             .catch((error) => console.log(error));
-    }
+    }, []);
 
-    render() {
-        return (
-            <div>
-                {this.state.items.map(({ content = "", link = "", title = "" }: Items, index: number) => (
-                    <EmbeddedPenPortfolio
-                        key={`${index}_${title}`}
-                        content={content}
-                        link={link}
-                        title={title}
-                        isScriptLoaded={this.state.loaded}
-                    />
-                ))}
-            </div>
-        );
-    }
-}
-
-export default withStyles(styles)(EmbeddedPenPortfolios);
+    return (
+        <div>
+            {items.map(({ content = "", link = "", title = "" }: Items, index: number) => (
+                <EmbeddedPenPortfolio
+                    key={`${index}_${title}`}
+                    content={content}
+                    link={link}
+                    title={title}
+                    isScriptLoaded={loaded}
+                />
+            ))}
+        </div>
+    );
+};
