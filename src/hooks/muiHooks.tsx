25bed51b76f8a4b8ca78bb4b78d5c9fcbef6457e
HeySP6 <h1017760@connect.hku.hk>
2021-11-15 00:44:56 +0100

    added `useWidth` hooks from `material-ui`

---
+++ src/hooks/muiHooks.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Theme, ThemeProvider, useTheme, createTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { Breakpoint } from '@material-ui/core/styles/createBreakpoints';
+
+type BreakpointOrNull = Breakpoint | null;
+
+/**
+ * Be careful using this hook. It only works because the number of
+ * breakpoints in theme is static. It will break once you change the number of
+ * breakpoints. See https://reactjs.org/docs/hooks-rules.html#only-call-hooks-at-the-top-level
+ */
+// copied from https://v4.mui.com/components/use-media-query/#migrating-from-withwidth
+export function useWidth() {
+    const theme: Theme = useTheme();
+    const keys: Breakpoint[] = [...theme.breakpoints.keys].reverse();
+    return (
+        keys.reduce((output: BreakpointOrNull, key: Breakpoint) => {
+            // eslint-disable-next-line react-hooks/rules-of-hooks
+            const matches = useMediaQuery(theme.breakpoints.up(key));
+            return !output && matches ? key : output;
+        }, null) || 'xs'
+    );
+}
