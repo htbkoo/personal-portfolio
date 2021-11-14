799b1a7822fb51f2a7539c339ea982027c29107d
HeySP6 <h1017760@connect.hku.hk>
2021-11-15 00:00:31 +0100

    applied auto-formatting by `prettier`

--- src/components/common/Section.tsx
+++ src/components/common/Section.tsx
@@ -17,47 +17,53 @@ const styles = (theme: Theme) =>
     });
 
 interface SectionProps extends WithStyles<typeof styles> {
-    id: string,
-    title?: string,
-    subtitle?: string,
-    hasDivider?: boolean,
+    id: string;
+    title?: string;
+    subtitle?: string;
+    hasDivider?: boolean;
 }
 
-const Section: FunctionComponent<SectionProps> = ({id, title, subtitle, hasDivider, children, classes}) => (
+const Section: FunctionComponent<SectionProps> = ({ id, title, subtitle, hasDivider, children, classes }) => (
     <div id={id} className={classes.section}>
         <div className={classes.header}>
             {optionalTitle(title)}
             {optionalSubtitle(subtitle)}
         </div>
         {optionalDivider(hasDivider)}
-        <div className={classes.body}>
-            {children}
-        </div>
+        <div className={classes.body}>{children}</div>
     </div>
 );
 
 function optionalTitle(title?: string) {
-    return optionalTypography({text: title, variant: "h3"});
+    return optionalTypography({ text: title, variant: "h3" });
 }
 
 function optionalSubtitle(subtitle?: string) {
-    return optionalTypography({text: subtitle, variant: "h5"});
+    return optionalTypography({ text: subtitle, variant: "h5" });
 }
 
-function optionalTypography({text, variant}: { text?: string, variant: TypographyTypeMap["props"]["variant"] }) {
-    return text
-        ? <Typography variant={variant} paragraph>{text}</Typography>
-        : "";
+function optionalTypography({
+    text,
+    variant,
+}: {
+    text?: string;
+    variant: TypographyTypeMap["props"]["variant"];
+}) {
+    return text ? (
+        <Typography variant={variant} paragraph>
+            {text}
+        </Typography>
+    ) : (
+        ""
+    );
 }
 
 function optionalDivider(hasDivider?: boolean) {
-    return hasDivider
-        ? <Divider/>
-        : "";
+    return hasDivider ? <Divider /> : "";
 }
 
 Section.defaultProps = {
-    hasDivider: false
+    hasDivider: false,
 };
 
 export default withStyles(styles)(Section);
