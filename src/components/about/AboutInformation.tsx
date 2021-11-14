544dc3d02d89cbbc962c461fb154129cd4d945d5
HeySP6 <h1017760@connect.hku.hk>
2021-11-14 23:58:45 +0100

    refactored `<AboutInformation/>` for `material-ui` usage to migrate away to `makeStyles` from `withStyles` for consistency

--- src/components/about/AboutInformation.tsx
+++ src/components/about/AboutInformation.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { createStyles, Theme, withStyles, WithStyles } from "@material-ui/core";
+import { makeStyles, Theme } from "@material-ui/core";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -10,31 +10,28 @@ import Paper from "@material-ui/core/Paper";
 import informationMetadatas from "../../metadata/about/information.json";
 import AboutEntry from "../../model/AboutEntry";
 
-const styles = (theme: Theme) =>
-    createStyles({
-        root: {
-            // width: '100%',
-            marginTop: theme.spacing(3),
-            overflowX: "auto",
-        },
-        table: {
-            height: "auto",
-            // minWidth: 700,
-        },
-        row: {
-            color: theme.palette.secondary.contrastText,
-            height: theme.spacing(4),
-        },
-        divider: {
-            backgroundColor: theme.palette.secondary.light,
-            color: theme.palette.secondary.contrastText,
-        },
-    });
+const useStyles = makeStyles((theme: Theme) => ({
+    root: {
+        // width: '100%',
+        marginTop: theme.spacing(3),
+        overflowX: "auto",
+    },
+    table: {
+        height: "auto",
+        // minWidth: 700,
+    },
+    row: {
+        color: theme.palette.secondary.contrastText,
+        height: theme.spacing(4),
+    },
+    divider: {
+        backgroundColor: theme.palette.secondary.light,
+        color: theme.palette.secondary.contrastText,
+    },
+}));
 
-interface AboutInformationProps extends WithStyles<typeof styles> {}
-
-function AboutInformation(props: AboutInformationProps) {
-    const { classes } = props;
+export default () => {
+    const classes = useStyles();
     return (
         <Paper className={classes.root}>
             <Table className={classes.table}>
@@ -74,6 +71,4 @@ function AboutInformation(props: AboutInformationProps) {
             </TableRow>
         );
     }
-}
-
-export default withStyles(styles)(AboutInformation);
+};
