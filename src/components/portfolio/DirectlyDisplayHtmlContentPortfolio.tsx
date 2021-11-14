4f7576ae97ba9a57e188eeec200e8500f92e749f
HeySP6 <h1017760@connect.hku.hk>
2021-11-15 00:38:02 +0100

    cleaned up

--- src/components/portfolio/DirectlyDisplayHtmlContentPortfolio.tsx
+++ src/components/portfolio/DirectlyDisplayHtmlContentPortfolio.tsx
@@ -1,21 +1,17 @@
 import * as React from "react";
-import { createStyles, Theme, withStyles, WithStyles } from "@material-ui/core";
+
 import { PortfolioProps } from "./PortfolioProps";
 
-const styles = (theme: Theme) => createStyles({});
+interface DirectlyDisplayHtmlContentPortfolioProps extends PortfolioProps {}
 
-interface DirectlyDisplayHtmlContentPortfolioProps extends PortfolioProps, WithStyles<typeof styles> {}
-
-function DirectlyDisplayHtmlContentPortfolio(props: DirectlyDisplayHtmlContentPortfolioProps) {
+export default (props: DirectlyDisplayHtmlContentPortfolioProps) => {
     const { title, link, content } = props;
     return (
         <div>
             <div>
                 <a href={link}>{title}</a>
             </div>
-            <div dangerouslySetInnerHTML={{ __html: content }}/>
+            <div dangerouslySetInnerHTML={{ __html: content }} />
         </div>
     );
-}
-
-export default withStyles(styles)(DirectlyDisplayHtmlContentPortfolio);
+};
