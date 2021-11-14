4cbe35f5a94a0ba10aa54c9519e464db0d7c4b78
HeySP6 <h1017760@connect.hku.hk>
2021-11-15 00:27:43 +0100

    refactored `<DrawerItemsWithScrollspy/>` for `material-ui` usage to migrate away to `makeStyles` from `withStyles` for consistency

--- src/components/page/DrawerItemsWithScrollspy.tsx
+++ src/components/page/DrawerItemsWithScrollspy.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Theme, withStyles } from "@material-ui/core/styles";
-import { createStyles, Link, WithStyles } from "@material-ui/core";
+import { Theme } from "@material-ui/core/styles";
+import { Link, makeStyles } from "@material-ui/core";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
@@ -13,48 +13,48 @@ import OldVersionLinkButton from "./OldVersionLinkButton";
 
 const drawerWidth = 240;
 
-const styles = (theme: Theme) =>
-    createStyles({
-        root: {
-            display: "flex",
-        },
-        drawer: {
-            width: drawerWidth,
-            flexShrink: 0,
-        },
-        drawerPaper: {
-            width: drawerWidth,
-        },
-        content: {
-            flexGrow: 1,
-            padding: theme.spacing(3),
-        },
-        toolbar: theme.mixins.toolbar,
-        scrollSpyList: {
-            margin: 0,
-            padding: 0,
-        },
-        scrollSpyListItem: {
-            display: "flex",
-        },
-        oldVersionLinkButton: {
-            margin: "5%",
-        },
-        isCurrent: {
-            fontWeight: "bolder",
-            color: theme.palette.primary.contrastText,
-            backgroundColor: theme.palette.primary.light,
-        },
-    });
+const useStyles = makeStyles((theme: Theme) => ({
+    root: {
+        display: "flex",
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+    },
+    toolbar: theme.mixins.toolbar,
+    scrollSpyList: {
+        margin: 0,
+        padding: 0,
+    },
+    scrollSpyListItem: {
+        display: "flex",
+    },
+    oldVersionLinkButton: {
+        margin: "5%",
+    },
+    isCurrent: {
+        fontWeight: "bolder",
+        color: theme.palette.primary.contrastText,
+        backgroundColor: theme.palette.primary.light,
+    },
+}));
 
-interface DrawerItemsWithScrollspyProps extends WithStyles<typeof styles> {
+interface DrawerItemsWithScrollspyProps {
     items: string[];
 }
 
 const EMPIRICAL_OFFSET = -80;
 
-function DrawerItemsWithScrollspy(props: DrawerItemsWithScrollspyProps) {
-    const { items, classes } = props;
+export default (props: DrawerItemsWithScrollspyProps) => {
+    const classes = useStyles();
+    const { items } = props;
 
     return (
         <React.Fragment>
@@ -84,10 +84,8 @@ function DrawerItemsWithScrollspy(props: DrawerItemsWithScrollspyProps) {
             </Hidden>
         </React.Fragment>
     );
-}
+};
 
 function itemToHref(item: string): string {
     return `#${item.replace(/ /g, "-").toLowerCase()}`;
 }
-
-export default withStyles(styles)(DrawerItemsWithScrollspy);
