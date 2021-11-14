e65c8105d696f328176e77bfe69e22779d40a860
HeySP6 <h1017760@connect.hku.hk>
2021-11-15 00:12:23 +0100

    refactored `<Section/>` for `material-ui` usage to migrate away to `makeStyles` from `withStyles` for consistency

--- src/components/common/Section.tsx
+++ src/components/common/Section.tsx
@@ -1,38 +1,40 @@
 import * as React from "react";
 import { FunctionComponent } from "react";
-import { createStyles, Theme, TypographyTypeMap, withStyles, WithStyles } from "@material-ui/core";
+import { makeStyles, Theme, TypographyTypeMap } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 
 import { getStylesToFixPageHeaderOverlappingInPageAnchorIssue } from "../../utils/cssUtils";
 
-const styles = (theme: Theme) =>
-    createStyles({
-        section: {
-            padding: theme.spacing(3),
-            ...getStylesToFixPageHeaderOverlappingInPageAnchorIssue(theme),
-        },
-        header: {},
-        body: {},
-    });
+const useStyles = makeStyles((theme: Theme) => ({
+    section: {
+        padding: theme.spacing(3),
+        ...getStylesToFixPageHeaderOverlappingInPageAnchorIssue(theme),
+    },
+    header: {},
+    body: {},
+}));
 
-interface SectionProps extends WithStyles<typeof styles> {
+interface SectionProps {
     id: string;
     title?: string;
     subtitle?: string;
     hasDivider?: boolean;
 }
 
-const Section: FunctionComponent<SectionProps> = ({ id, title, subtitle, hasDivider, children, classes }) => (
-    <div id={id} className={classes.section}>
-        <div className={classes.header}>
-            {optionalTitle(title)}
-            {optionalSubtitle(subtitle)}
+const Section: FunctionComponent<SectionProps> = ({ id, title, subtitle, hasDivider, children }) => {
+    const classes = useStyles();
+    return (
+        <div id={id} className={classes.section}>
+            <div className={classes.header}>
+                {optionalTitle(title)}
+                {optionalSubtitle(subtitle)}
+            </div>
+            {optionalDivider(hasDivider)}
+            <div className={classes.body}>{children}</div>
         </div>
-        {optionalDivider(hasDivider)}
-        <div className={classes.body}>{children}</div>
-    </div>
-);
+    );
+};
 
 function optionalTitle(title?: string) {
     return optionalTypography({ text: title, variant: "h3" });
@@ -66,4 +68,4 @@ Section.defaultProps = {
     hasDivider: false,
 };
 
-export default withStyles(styles)(Section);
+export default Section;
