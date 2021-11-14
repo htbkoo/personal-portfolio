af5fe8647bdc746be1dd4200a10afe93faf35e45
HeySP6 <h1017760@connect.hku.hk>
2021-11-15 00:34:52 +0100

    applied auto-formatting by `prettier`

--- src/components/portfolio/DirectlyDisplayHtmlContentPortfolio.tsx
+++ src/components/portfolio/DirectlyDisplayHtmlContentPortfolio.tsx
@@ -1,21 +1,19 @@
 import * as React from "react";
-import {createStyles, Theme, withStyles, WithStyles} from "@material-ui/core";
-import {PortfolioProps} from "./PortfolioProps";
+import { createStyles, Theme, withStyles, WithStyles } from "@material-ui/core";
+import { PortfolioProps } from "./PortfolioProps";
 
 const styles = (theme: Theme) => createStyles({});
 
-interface DirectlyDisplayHtmlContentPortfolioProps extends PortfolioProps, WithStyles<typeof styles> {
-}
+interface DirectlyDisplayHtmlContentPortfolioProps extends PortfolioProps, WithStyles<typeof styles> {}
 
 function DirectlyDisplayHtmlContentPortfolio(props: DirectlyDisplayHtmlContentPortfolioProps) {
-    const {title, link, content} = props;
+    const { title, link, content } = props;
     return (
         <div>
             <div>
                 <a href={link}>{title}</a>
             </div>
-            <div dangerouslySetInnerHTML={{__html: content}}>
-            </div>
+            <div dangerouslySetInnerHTML={{ __html: content }}></div>
         </div>
     );
 }
