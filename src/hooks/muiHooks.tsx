60b486a3ca33bedd057319aedfda160257a9fef8
HeySP6 <h1017760@connect.hku.hk>
2021-11-21 22:33:28 +0100

    refactored to eliminate warning

--- src/hooks/muiHooks.tsx
+++ src/hooks/muiHooks.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Theme, useTheme } from "@material-ui/core/styles";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { Breakpoint } from "@material-ui/core/styles/createBreakpoints";
