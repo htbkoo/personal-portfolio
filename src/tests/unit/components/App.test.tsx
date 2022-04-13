5b5cf9dd8e03144f226bb34024b69880957d02e0
HeySP6 <h1017760@connect.hku.hk>
2022-04-14 01:55:56 +0200

    (migrate-to-next-js) updated to Client Rendering APIs from `react 18` for tests to eliminate warnings (reference: https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis)

--- src/tests/unit/components/App.test.tsx
+++ src/tests/unit/components/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from '../../../components/App';
 import { overrideGoogleAnalyticsRelatedProcessEnv } from "../../utils/utils";
 
@@ -8,15 +8,20 @@ describe('<App/>', function () {
         overrideGoogleAnalyticsRelatedProcessEnv({ trackingEnabled: "false" });
 
         const div = document.createElement('div');
-        ReactDOM.render(<App/>, div);
-        ReactDOM.unmountComponentAtNode(div);
+        // @ts-ignore: TODO: remove this once @types/react and @types/react-dom is upgraded to v18
+        const root = createRoot(div!);
+        root.render(<App/>);
+        root.unmount();
     });
 
     it('with GA enabled, renders without crashing', () => {
         overrideGoogleAnalyticsRelatedProcessEnv({ trackingEnabled: "true", trackingId: "someId", testMode: "true" });
 
         const div = document.createElement('div');
-        ReactDOM.render(<App/>, div);
-        ReactDOM.unmountComponentAtNode(div);
+
+        // @ts-ignore: TODO: remove this once @types/react and @types/react-dom is upgraded to v18
+        const root = createRoot(div!);
+        root.render(<App/>);
+        root.unmount();
     });
 });
