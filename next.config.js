e0ef3930fc53a09e5419bb415289a796afa07785
HeySP6 <h1017760@connect.hku.hk>
2022-04-13 20:50:47 +0200

    (migrate-to-next-js) added config to handle the subpath in GitHub Page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const gitHubPageSubpath = "personal-portfolio";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+
+    // reference: https://dev.to/jameswallis/deploying-a-next-js-app-to-github-pages-24pn
+    basePath: `/${gitHubPageSubpath}`,
+    assetPrefix: `/${gitHubPageSubpath}/`,
 };
 
 module.exports = nextConfig;
