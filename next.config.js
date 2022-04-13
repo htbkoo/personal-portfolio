e2b24cd837a73f571d5243bda1cf348abdea59d5
HeySP6 <h1017760@connect.hku.hk>
2022-04-13 22:55:23 +0200

    (migrate-to-next-js) updated `next.config.js` for `basePath` and `assetPrefix` (reference: https://blog.sallai.me/deploy-next-site-to-github-pages)

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
-const gitHubPageSubpath = "personal-portfolio";
-
 // reference: https://nextjs.org/docs/api-reference/next.config.js/cdn-support-with-asset-prefix
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === "production";
+
+// reference: https://blog.sallai.me/deploy-next-site-to-github-pages
+const basePath = isProd ? "/personal-portfolio" : "";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
 
     // reference: https://nextjs.org/docs/api-reference/next.config.js/basepath
-    basePath: `/${gitHubPageSubpath}`,
+    basePath,
     // Use the GitHub Pages subpath in production and localhost for development.
-    assetPrefix: isProd ? `/${gitHubPageSubpath}/` : '',
+    assetPrefix: `${basePath}/`,
 };
 
 module.exports = nextConfig;
