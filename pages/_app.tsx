efc7294486d32778fad0c0ff100e056ef4560689
HeySP6 <h1017760@connect.hku.hk>
2022-04-14 02:19:22 +0200

    (migrate-to-next-js) moved import of `@fontsource/roboto` from legacy `index.tsx` to `_app.tsx`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+// reference:
+// 1. https://material-ui.com/components/typography/#install-with-npm
+// 2. https://fontsource.org/docs/guides/nextjs
+import "@fontsource/roboto";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
