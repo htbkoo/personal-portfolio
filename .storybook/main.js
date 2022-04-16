9547cffaf09ea09059f6e117db691f5fbc16212d
HeySP6 <h1017760@connect.hku.hk>
2022-04-16 03:30:44 +0200

    attempted to fix broken `storybook` local run by upgrading the webpack-builder to webpack 5
    references:
    1. https://gist.github.com/shilman/8856ea1786dcd247139b47b270912324#upgrading-from-webpack-4
    2. https://storybook.js.org/blog/storybook-for-webpack-5/
    3. https://github.com/RyanClementsHax/storybook-addon-next/issues/18#issuecomment-1030857277
    4. https://stackoverflow.com/questions/69159274/angular-storybook-webpackoptionsvalidationerror-webpack-has-been-initialised

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,5 +7,13 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     'storybook-addon-next'
-  ]
+  ],
+  "core": {
+    // references:
+    // 1. https://gist.github.com/shilman/8856ea1786dcd247139b47b270912324#upgrading-from-webpack-4
+    // 2. https://storybook.js.org/blog/storybook-for-webpack-5/
+    // 3. https://github.com/RyanClementsHax/storybook-addon-next/issues/18#issuecomment-1030857277
+    // 4. https://stackoverflow.com/questions/69159274/angular-storybook-webpackoptionsvalidationerror-webpack-has-been-initialised
+    "builder": "webpack5",
+  },
 }
