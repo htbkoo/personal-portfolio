ba9f8754479639b34bf0b98d25af047852902c8f
htbkoo <h1017760+github@connect.hku.hk>
2023-06-17 16:49:18 +0200

    fixed broken image rendering after upgrading to `next` `v13`

--- src/components/common/GitHubMarkImg.tsx
+++ src/components/common/GitHubMarkImg.tsx
@@ -2,9 +2,6 @@ import * as React from "react";
 import { useTheme } from "@material-ui/core/styles";
 import Image from "next/image";
 
-const GitHubMark120Plus = require("@/public/GitHub-Mark/PNG/GitHub-Mark-120px-plus.png").default;
-const GitHubMarkLight120Plus = require("@/public/GitHub-Mark/PNG/GitHub-Mark-Light-120px-plus.png").default;
-
 const DEFAULT_MAX_ICON_SIZE = 16;
 const DEFAULT_IS_LIGHT = "black";
 
@@ -26,19 +23,16 @@ export function GitHubMarkImg({
     color = DEFAULT_IS_LIGHT,
 }: GitHubMarkImgProps) {
     const src = getGitHubMarkImgSrc(color);
-    return <Image
-        src={src}
-        alt="github-htbkoo-personal-portfolio"
-        width={size}
-        height={size}
-    />;
+    return <Image src={src} alt="github-htbkoo-personal-portfolio" width={size} height={size} />;
 }
 
 export function getGitHubMarkImgSrc(color: GitHubMarkImgProps["color"]) {
-    return color === "black" ? GitHubMark120Plus : GitHubMarkLight120Plus;
+    return color === "black"
+        ? "/static/GitHub-Mark/PNG/GitHub-Mark-120px-plus.png"
+        : "/static/GitHub-Mark/PNG/GitHub-Mark-Light-120px-plus.png";
 }
 
-export function useGitHubMarkImgColorBasedOnTheme(){
+export function useGitHubMarkImgColorBasedOnTheme() {
     const theme = useTheme();
-    return theme.palette.type === "dark" ? "white" : "black"
+    return theme.palette.type === "dark" ? "white" : "black";
 }
