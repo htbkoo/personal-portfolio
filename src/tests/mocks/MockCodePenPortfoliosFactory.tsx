b42d96abf857b9d5451cfb29f24a73c90ba56a19
htbkoo <h1017760+github@connect.hku.hk>
2023-07-30 21:35:05 +0200

    added `key` prop to component to eliminate warnings

--- src/tests/mocks/MockCodePenPortfoliosFactory.tsx
+++ src/tests/mocks/MockCodePenPortfoliosFactory.tsx
@@ -1,15 +1,13 @@
 import * as React from "react";
 
-import {PortfoliosFactory} from "../../components/portfolio/PortfoliosFactory";
+import { PortfoliosFactory } from "../../components/portfolio/PortfoliosFactory";
 import DirectlyDisplayHtmlContentPortfolio from "../../components/portfolio/DirectlyDisplayHtmlContentPortfolio";
 import mockCodePenRssFeeds from "./mockCodePenRssFeeds";
 
 export default class MockCodePenPortfoliosFactory implements PortfoliosFactory {
     createPortfolios() {
         return Promise.resolve(
-            mockCodePenRssFeeds.map(
-                feed => (<DirectlyDisplayHtmlContentPortfolio {...feed}/>)
-            )
+            mockCodePenRssFeeds.map((feed, i) => <DirectlyDisplayHtmlContentPortfolio key={i} {...feed} />),
         );
     }
 }
