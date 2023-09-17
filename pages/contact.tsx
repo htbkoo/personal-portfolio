06b925ea09fa441ade25d4bd348e8ac244d3dc88
htbkoo <h1017760+github@connect.hku.hk>
2023-09-17 19:13:56 +0200

    refactored

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 import type { NextPage } from "next";
 
-import { getSectionConfig } from "@/src/metadata/sectionConfigs";
+import { sectionConfigs } from "@/src/metadata/sectionConfigs";
 import PageSection from "@/src/components/page/PageSection";
 
 const Contact: NextPage = () => {
-    return <PageSection config={getSectionConfig("contact")!} />;
+    return <PageSection config={sectionConfigs.contact} />;
 };
 
 export default Contact;
