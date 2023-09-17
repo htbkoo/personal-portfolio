b4e20fc5dbd2e79a5e17993d07a9e06c9397f381
htbkoo <h1017760+github@connect.hku.hk>
2023-09-17 14:48:27 +0200

    refactor `sectionConfigs.tsx`

--- pages/exercise.tsx
+++ pages/exercise.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 import type { NextPage } from "next";
 
-import sectionConfigs from "@/src/metadata/sectionConfigs";
+import { getSectionConfig } from "@/src/metadata/sectionConfigs";
 import PageSection from "@/src/components/page/PageSection";
 
 const Exercise: NextPage = () => {
-    return <PageSection config={sectionConfigs.exercise} />;
+    return <PageSection config={getSectionConfig("exercise")} />;
 };
 
 export default Exercise;
