36d565c6935550cfd413e06c9aa767ca53258ceb
htbkoo <htbkoo+github@proton.me>
2023-09-24 00:38:33 +0200

    feat(EmbeddedPenExercise): Update design and add title to page

--- src/components/exercises/EmbeddedPenExercise.tsx
+++ src/components/exercises/EmbeddedPenExercise.tsx
@@ -1,20 +1,19 @@
 import * as React from "react";
 import makeStyles from '@mui/styles/makeStyles';
 import { Breakpoint } from '@mui/material/styles';
-import CodePen from "ts-react-codepen-embed";
 import CircularProgress from "@mui/material/CircularProgress";
+import CodePen from "ts-react-codepen-embed";
 
 import { ExerciseProps } from "./ExerciseProps";
 import CodePenItemContentParser from "@/src/services/exercises/CodePenItemContentParser";
 import { credentialsExtractor } from "@/src/services/exercises/CodePenItemContentExtractor";
 import { useWidth } from "../../hooks/muiHooks";
 import { useCodepenEmbedScriptTag } from "@/src/hooks/useCodepenEmbedScriptTag";
+import Section from "@/src/components/common/Section";
 
 const useStyles = makeStyles(
     () => ({
-        embeddedContainer: {
-            margin: "5%",
-        },
+        embeddedContainer: {},
     }),
     { name: "MuiMyEmbeddedPenPortfolio" },
 );
@@ -37,18 +36,20 @@ const EmbeddedPenExercise = ({ title, content }: ExerciseProps) => {
     const { user, hash } = contentParser.parseContent(credentialsExtractor);
 
     return (
-        <div className={classes.embeddedContainer}>
-            <CodePen
-                user={user}
-                hash={hash}
-                title={title}
-                height={MAPPING_HEIGHTS[width]}
-                loader={() => <CircularProgress />}
-                defaultTab="result"
-                shouldLoadScript={false}
-                overrideAsLoaded={loaded}
-            />
-        </div>
+        <Section id="codePenExercise" hasDivider={true} title={title}>
+            <div className={classes.embeddedContainer}>
+                <CodePen
+                    user={user}
+                    hash={hash}
+                    title={title}
+                    height={MAPPING_HEIGHTS[width]}
+                    loader={() => <CircularProgress />}
+                    defaultTab="result"
+                    shouldLoadScript={false}
+                    overrideAsLoaded={loaded}
+                />
+            </div>
+        </Section>
     );
 };
 export default EmbeddedPenExercise;
