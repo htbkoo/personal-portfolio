e93babf890400efc7d873bf824ea056855a1def2
htbkoo <htbkoo+github@proton.me>
2024-05-28 18:35:13 +0200

    chore: Update formatting

--- src/components/page/OldVersionLinkButton.tsx
+++ src/components/page/OldVersionLinkButton.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Theme } from "@mui/material/styles";
-import makeStyles from '@mui/styles/makeStyles';
+import makeStyles from "@mui/styles/makeStyles";
 import Button from "@mui/material/Button";
 
-const useStyles = makeStyles((theme: Theme) => ({
-    button: {
-        marginLeft: theme.spacing(1),
-    },
-}), {name: "MuiMyOldVersionLinkButton"});
+const useStyles = makeStyles(
+    (theme: Theme) => ({
+        button: {
+            marginLeft: theme.spacing(1),
+        },
+    }),
+    { name: "MuiMyOldVersionLinkButton" },
+);
 
 const OLD_VERSION_URL = "https://codepen.io/htbkoo/";
 
