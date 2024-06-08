e152ab8baceaa1ef8969d0906e6643ec02e749d8
htbkoo <htbkoo+github@proton.me>
2024-06-08 14:02:03 +0200

    feat(SimpleSubPage): Refactor

--- pages/exercises/[exercise].tsx
+++ pages/exercises/[exercise].tsx
@@ -1,24 +1,13 @@
 import React from "react";
 import dynamic from "next/dynamic";
-import CircularProgress from "@mui/material/CircularProgress";
 
 import type { NextPage } from "next";
 
 import { sectionConfigs } from "@/src/metadata/sectionConfigs";
-import PageSection from "@/src/components/page/PageSection";
-import { useSubPagesDataForCurrPath } from "@/src/hooks/useSubPagesDataForCurrPath";
+import { SimpleSubPage } from "@/src/components/common/SimpleSubPage";
 
 const Exercise = () => {
-    const { data: pageData, loading } = useSubPagesDataForCurrPath(sectionConfigs.exercises);
-
-    if (loading) {
-        return <CircularProgress />;
-    }
-    if (pageData) {
-        return <PageSection config={pageData} />;
-    }
-
-    return null;
+    return <SimpleSubPage configs={sectionConfigs.exercises} />;
 };
 
 const NoSsrExercise: NextPage = dynamic(async () => Exercise, { ssr: false });
--- pages/tools/[tool].tsx
+++ pages/tools/[tool].tsx
@@ -1,24 +1,13 @@
 import React from "react";
 import dynamic from "next/dynamic";
-import CircularProgress from "@mui/material/CircularProgress";
 
 import type { NextPage } from "next";
 
 import { sectionConfigs } from "@/src/metadata/sectionConfigs";
-import PageSection from "@/src/components/page/PageSection";
-import { useSubPagesDataForCurrPath } from "@/src/hooks/useSubPagesDataForCurrPath";
+import { SimpleSubPage } from "@/src/components/common/SimpleSubPage";
 
 const Tool = () => {
-    const { data: pageData, loading } = useSubPagesDataForCurrPath(sectionConfigs.tools);
-
-    if (loading) {
-        return <CircularProgress />;
-    }
-    if (pageData) {
-        return <PageSection config={pageData} />;
-    }
-
-    return null;
+    return <SimpleSubPage configs={sectionConfigs.tools} />;
 };
 
 const NoSsrTool: NextPage = dynamic(async () => Tool, { ssr: false });
