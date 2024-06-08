4465dac80ca2bdcd9ccc40f13f96b6cdc727579d
htbkoo <htbkoo+github@proton.me>
2024-06-08 13:53:37 +0200

    feat(useSubPagesDataForCurrPath): Refactor to reuse code

--- src/hooks/useSubPagesDataForCurrPath.ts
+++ src/hooks/useSubPagesDataForCurrPath.ts
@@ -1,31 +1,30 @@
-import { useEffect, useState } from "react";
-
 import { useAppGlobalStateContext } from "@/src/contexts/AppGlobalStateContext";
 import SectionMetadata from "@/src/model/SectionMetadata";
+import { useSubPages } from "@/src/hooks/useSubPages";
 
+/**
+ * This hook loads the SubPages metadata from the `currDisplayPath` from {@link useAppGlobalStateContext}
+ *
+ * @param configs
+ */
 export const useSubPagesDataForCurrPath = (configs: SectionMetadata) => {
     const { currDisplayPath } = useAppGlobalStateContext();
 
+    const {
+        data: subPagesData,
+        loading: subPagesLoading,
+        error,
+    } = useSubPages({ getSubPages: configs.getSubPages });
+
     const pagePath = currDisplayPath.split("/").at(-2);
 
-    const [loading, setLoading] = useState(true);
-    const [pageData, setPageData] = useState<SectionMetadata | undefined>(undefined);
+    if (subPagesLoading) {
+        return { data: null, loading: subPagesLoading, error: null };
+    }
 
-    useEffect(() => {
-        if (!configs.getSubPages) {
-            setLoading(false);
-        } else {
-            configs.getSubPages().then(({ data }) => {
-                setLoading(false);
+    if (!pagePath || !subPagesData || !subPagesData[pagePath]) {
+        return { data: null, loading: false, error };
+    }
 
-                if (!pagePath || !data || !data[pagePath]) {
-                    return;
-                }
-
-                setPageData(data[pagePath]);
-            });
-        }
-    }, [pagePath, configs]);
-
-    return { data: pageData, loading };
+    return { data: subPagesData[pagePath], loading: false, error };
 };
