771b0cded594180c49268261ed5fd480c85d16fa
htbkoo <htbkoo+github@proton.me>
2024-06-09 22:25:14 +0200

    feat(Tools): Remove NoSSR from `[exercise].tsx` page

--- pages/exercises/[exercise].tsx
+++ pages/exercises/[exercise].tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import dynamic from "next/dynamic";
-
-import type { NextPage } from "next";
 
 import { sectionConfigs } from "@/src/metadata/sectionConfigs";
 import { SimpleSubPage } from "@/src/components/common/SimpleSubPage";
@@ -10,6 +7,4 @@ const Exercise = () => {
     return <SimpleSubPage configs={sectionConfigs.exercises} />;
 };
 
-const NoSsrExercise: NextPage = dynamic(async () => Exercise, { ssr: false });
-
-export default NoSsrExercise;
+export default Exercise;
