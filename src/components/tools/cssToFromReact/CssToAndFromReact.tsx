85279c0ba508f2d10001e8ac2163d0664ff2cbaa
htbkoo <htbkoo+github@proton.me>
2024-06-09 18:25:53 +0200

    feat(CssToAndFromReact): Add `<CssToAndFromReact/>` (wip)

--- src/components/tools/cssToFromReact/CssToAndFromReact.tsx
+++ src/components/tools/cssToFromReact/CssToAndFromReact.tsx
@@ -3,14 +3,25 @@ import { type TextareaHTMLAttributes } from "react";
 import makeStyles from "@mui/styles/makeStyles";
 import CircularProgress from "@mui/material/CircularProgress";
 import { TextareaAutosize } from "@mui/base/TextareaAutosize";
+import Image from "next/image";
 
 import Section from "../../common/Section";
 import { AsyncStateType } from "@/src/utils/types";
+import { withStaticPrefix } from "@/src/utils/assetUtils";
+import Link, { type LinkProps } from "@mui/material/Link";
 
 const useStyles = makeStyles(
     (theme) => ({
         container: {
             marginTop: theme.spacing(4),
+        },
+        header: {
+            textAlign: "center",
+        },
+        logo: {
+            maxWidth: "100%",
+            height: "auto",
+            width: "auto",
         },
         loadingSpinner: {
             marginTop: theme.spacing(2),
@@ -24,37 +35,31 @@ const useStyles = makeStyles(
     { name: "MuiMyEmbeddedPenPortfolio" },
 );
 
-const useImport = <T,>(modulePath: string) => {
-    const [state, setState] = React.useState<AsyncStateType<T>>({
+type CssToAndFromReactModuleType = typeof import("css-to-and-from-react");
+
+const useCssToAndFromReact = () => {
+    const [state, setState] = React.useState<AsyncStateType<CssToAndFromReactModuleType>>({
         loading: true,
     });
 
     React.useEffect(() => {
-        console.log(`DEBUGDEBUG -- importing in useEffect in useImport`)
-
-        import(modulePath)
+        import("css-to-and-from-react")
             .then((module) => {
-                console.log(`DEBUGDEBUG -- loaded in useEffect in useImport`)
-
                 setState({
                     loading: false,
                     data: module,
                 });
             })
             .catch((error) => {
-                console.log(`DEBUGDEBUG -- error in useEffect in useImport ${JSON.stringify(error)}`);
-
                 setState({
                     loading: false,
                     error,
                 });
             });
-    }, [modulePath]);
+    }, []);
 
     return { ...state };
 };
-
-type CssToAndFromReactModuleType = typeof import("css-to-and-from-react");
 
 const CssToAndFromReactConverter = () => {
     const classes = useStyles();
@@ -62,22 +67,15 @@ const CssToAndFromReactConverter = () => {
     const [cssText, setCssText] = React.useState("");
     const [reactText, setReactText] = React.useState("");
 
-    const { loading, data } = useImport<CssToAndFromReactModuleType>("css-to-and-from-react");
-    // const { loading, data } = useImport("../../common/Section");
-
-    import("react").then(() => {
-        console.log(`DEBUGDEBUG -- loaded css-to-and-from-react`)
-    }).catch(error=>{
-        console.warn(`DEBUGDEBUG -- ${error}`)
-
-    })
+    const { loading, data } = useCssToAndFromReact();
 
     const handleCssChange: TextareaHTMLAttributes<HTMLTextAreaElement>["onChange"] = React.useCallback(
         (event) => {
             const newCssText = event.target.value;
             setCssText(newCssText);
-            // @ts-ignore
-            setReactText(data?.transform(newCssText));
+            try {
+                setReactText(JSON.stringify(data?.transform(newCssText)));
+            } catch (error) {}
         },
         [data],
     );
@@ -85,10 +83,11 @@ const CssToAndFromReactConverter = () => {
         (event) => {
             const newReactText = event.target.value;
             setReactText(newReactText);
-            // @ts-ignore
-            data?.promiseReverse(newReactText).then((result) => {
-                setCssText(result.css);
-            }); // TODO: handle error
+            data?.promiseReverse(newReactText)
+                .then((result) => {
+                    setCssText(result.css);
+                })
+                .catch((error) => {}); // TODO: handle error
         },
         [data],
     );
@@ -104,9 +103,23 @@ const CssToAndFromReactConverter = () => {
 
     return (
         <div className={classes.converterContainer}>
-            <TextareaAutosize onChange={handleCssChange} value={reactText} />
+            <TextareaAutosize onChange={handleCssChange} value={cssText} />
             <TextareaAutosize onChange={handleReactStyleChange} value={reactText} />
         </div>
+    );
+};
+
+export const UnderlinedLink = ({
+    href,
+    children,
+}: {
+    href: LinkProps["href"];
+    children: LinkProps["children"];
+}) => {
+    return (
+        <Link color="inherit" underline="always" href={href} target="_blank" rel="noopener noreferrer">
+            {children}
+        </Link>
     );
 };
 
@@ -119,8 +132,45 @@ export const CssToAndFromReact = () => {
             hasDivider={true}
             title="A small tool to convert between CSS and React styles">
             <div className={classes.container}>
-                {/* TODO: add link to GitHub repo / npm page */}
-                <CssToAndFromReactConverter />
+                <header className={classes.header}>
+                    <Image
+                        className={classes.logo}
+                        src={withStaticPrefix("img/CssToAndFromReact-logo.svg")}
+                        alt="CSS to and from React logo"
+                        width={444}
+                        height={168}
+                    />
+                </header>
+                <p>
+                    This simple little tool is intended to help translate plain <code>CSS</code> into and back
+                    from the{" "}
+                    <UnderlinedLink href="https://facebook.github.io/react/tips/inline-styles.html">
+                        React in-line style
+                    </UnderlinedLink>{" "}
+                    specific JSON representation.
+                </p>
+                <p></p>
+                <p>Making it easy to copy and paste into an inline React component or a CSS stylesheet.</p>
+                <section>
+                    {/* TODO: add link to GitHub repo / npm page */}
+                    <CssToAndFromReactConverter />
+                </section>
+                <p>Hint: Just edit one of the textarea and the other would update accordingly.</p>
+                <footer>
+                    This tool is originally created by{" "}
+                    <UnderlinedLink href="http://staxmanade.com">Staxmanade</UnderlinedLink>, and is forked by{" "}
+                    <UnderlinedLink href="https://github.com/htbkoo">htbkoo</UnderlinedLink>.
+                    <br />
+                    (forked to{" "}
+                    <UnderlinedLink href="https://github.com/htbkoo/CssToAndFromReact">
+                        htbkoo/CssToAndFromReact
+                    </UnderlinedLink>{" "}
+                    from{" "}
+                    <UnderlinedLink href="https://github.com/staxmanade/CssToReact">
+                        staxmanade/CssToReact
+                    </UnderlinedLink>
+                    )
+                </footer>
             </div>
         </Section>
     );
